7209b91b988e10aaeb54600be057b186591f68fe
DilbarMirzaeva <dlbarmirza@gmail.com>
2025-11-16 18:44:22 +0400

    mapper

--- src/main/java/com/webapp/usermanagementservice/mapper/UserMapper.java
+++ src/main/java/com/webapp/usermanagementservice/mapper/UserMapper.java
@@ -1,4 +1,18 @@
 package com.webapp.usermanagementservice.mapper;
 
-public class UserMapper {
+import com.webapp.usermanagementservice.dto.UserRequest;
+import com.webapp.usermanagementservice.dto.UserResponse;
+import com.webapp.usermanagementservice.model.entity.User;
+import org.mapstruct.BeanMapping;
+import org.mapstruct.Mapper;
+import org.mapstruct.MappingTarget;
+import org.mapstruct.NullValuePropertyMappingStrategy;
+
+@Mapper(componentModel = "spring")
+public interface UserMapper {
+    UserResponse toDto(User user);
+    User toEntityFromRequest(UserRequest userRequest);
+
+    @BeanMapping(nullValuePropertyMappingStrategy = NullValuePropertyMappingStrategy.IGNORE)
+    void updateUser(UserRequest userRequest,@MappingTarget User user);
 }
