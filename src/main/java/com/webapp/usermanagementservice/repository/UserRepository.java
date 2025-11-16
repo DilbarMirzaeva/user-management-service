f39ab3f32ce923f7555926768af020c1e58740e9
DilbarMirzaeva <dlbarmirza@gmail.com>
2025-11-16 18:42:33 +0400

    repo

--- src/main/java/com/webapp/usermanagementservice/repository/UserRepository.java
+++ src/main/java/com/webapp/usermanagementservice/repository/UserRepository.java
@@ -1,8 +1,6 @@
 package com.webapp.usermanagementservice.repository;
 
 import com.webapp.usermanagementservice.model.entity.User;
-import jakarta.validation.constraints.Email;
-import jakarta.validation.constraints.NotBlank;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
