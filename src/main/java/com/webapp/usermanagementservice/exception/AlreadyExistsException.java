568301bdff0b3f02197b76be69524c54e013f6ce
DilbarMirzaeva <dlbarmirza@gmail.com>
2025-11-16 18:36:38 +0400

    add exception

---
+++ src/main/java/com/webapp/usermanagementservice/exception/AlreadyExistsException.java
@@ -0,0 +1,7 @@
+package com.webapp.usermanagementservice.exception;
+
+public class AlreadyExistsException extends RuntimeException {
+  public AlreadyExistsException(String message) {
+    super(message);
+  }
+}
