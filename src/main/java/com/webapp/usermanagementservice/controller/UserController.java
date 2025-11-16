9f180a684b0134f8c45d1ae96d94e49f9af082c8
DilbarMirzaeva <dlbarmirza@gmail.com>
2025-11-17 01:58:33 +0400

    UserController

--- src/main/java/com/webapp/usermanagementservice/controller/UserController.java
+++ src/main/java/com/webapp/usermanagementservice/controller/UserController.java
@@ -1,4 +1,55 @@
 package com.webapp.usermanagementservice.controller;
 
+import com.webapp.usermanagementservice.dto.UserRequest;
+import com.webapp.usermanagementservice.dto.UserResponse;
+import com.webapp.usermanagementservice.model.enums.Status;
+import com.webapp.usermanagementservice.service.UserService;
+import jakarta.validation.Valid;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.validation.annotation.Validated;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/api/v1/users")
+@RequiredArgsConstructor
+@Validated
 public class UserController {
+    private final UserService userService;
+
+    @PostMapping("/save")
+    public ResponseEntity<UserResponse> create(@Valid @RequestBody UserRequest request){
+        return ResponseEntity.status(HttpStatus.CREATED).body(userService.createUser(request));
+    }
+
+    @GetMapping("/all")
+    public ResponseEntity<List<UserResponse>> findAll(){
+        return ResponseEntity.ok(userService.getAllUsers());
+    }
+
+    @GetMapping("/{id}")
+    public ResponseEntity<UserResponse> findById(@PathVariable Long id){
+        return ResponseEntity.ok(userService.getUserById(id));
+    }
+
+    @PutMapping("/{id}")
+    public ResponseEntity<UserResponse> update(@PathVariable Long id,@Valid @RequestBody UserRequest userRequest){
+        return ResponseEntity.ok(userService.updateUser(id,userRequest));
+    }
+
+    @DeleteMapping("/{id}")
+    public ResponseEntity<Void> deleteById(@PathVariable Long id){
+        userService.deleteUserById(id);
+        return ResponseEntity.noContent().build();
+    }
+
+    @PatchMapping("/{id}")
+    public ResponseEntity<UserResponse> updateStatus(@PathVariable Long id,@Valid @RequestBody Status status){
+        return ResponseEntity.ok(userService.updateStatus(id,status));
+    }
+
+
 }
