837517eb9546087dc730017cb6a8ab879564bc7a
DilbarMirzaeva <dlbarmirza@gmail.com>
2025-11-17 19:31:19 +0400

    exception handler add method

--- src/main/java/com/webapp/usermanagementservice/exception/ErrorMessage.java
+++ src/main/java/com/webapp/usermanagementservice/exception/ErrorMessage.java
@@ -3,5 +3,6 @@ package com.webapp.usermanagementservice.exception;
 public enum ErrorMessage {
     ALREADY_EXISTS,
     USER_NOT_FOUND,
-    EXCEPTION
+    EXCEPTION,
+    METHOD_ARGUMENT_NOT_VALID
 }
--- src/main/java/com/webapp/usermanagementservice/exception/GlobalExceptionHandler.java
+++ src/main/java/com/webapp/usermanagementservice/exception/GlobalExceptionHandler.java
@@ -2,11 +2,13 @@ package com.webapp.usermanagementservice.exception;
 
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 
 import java.time.LocalDateTime;
 import java.util.UUID;
+import java.util.stream.Collectors;
 
 @ControllerAdvice
 public class GlobalExceptionHandler {
@@ -44,4 +46,26 @@ public class GlobalExceptionHandler {
                         .build());
     }
 
+    @ExceptionHandler(MethodArgumentNotValidException.class)
+    public ResponseEntity<ErrorResponse> handleMethodArgumentNotValid(MethodArgumentNotValidException e) {
+
+        String errors = e.getBindingResult().getFieldErrors().stream()
+                .map(err -> err.getField() + ": " + err.getDefaultMessage())
+                .collect(Collectors.joining("; "));
+
+        if (errors.isEmpty()) {
+            errors = "Validation Error";
+        }
+
+        ErrorResponse errorResponse = ErrorResponse.builder()
+                .error(ErrorMessage.METHOD_ARGUMENT_NOT_VALID)
+                .uuid(UUID.randomUUID())
+                .timestamp(LocalDateTime.now())
+                .message(errors)
+                .build();
+
+        return ResponseEntity
+                .status(HttpStatus.BAD_REQUEST)
+                .body(errorResponse);
+    }
 }
